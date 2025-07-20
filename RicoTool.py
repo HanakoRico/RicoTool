a93a98384e15a68fdca79ab2a27738289f37453c
HanakoRico <joeyc5410@gmail.com>
2025-07-20 06:25:47 -0500

    Update RicoTool.py

--- RicoTool.py
+++ RicoTool.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 from colorama import Fore, init
 
 init(autoreset=True)
@@ -7,14 +8,15 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 print(f'''
 {Fore.RED}
-██████╗░██╗░█████╗░░█████╗░
-██╔══██╗██║██╔══██╗██╔══██╗
-██████╔╝██║██║░░╚═╝██║░░██║
-██╔══██╗██║██║░░██╗██║░░██║
-██║░░██║██║╚█████╔╝╚█████╔╝
-╚═╝░░╚═╝╚═╝░╚════╝░░╚════╝░
+██╗  ██╗ ██████╗ ████████╗██╗     ██╗███╗   ██╗███████╗
+██║  ██║██╔═══██╗╚══██╔══╝██║     ██║████╗  ██║██╔════╝
+███████║██║   ██║   ██║   ██║     ██║██╔██╗ ██║█████╗  
+██╔══██║██║   ██║   ██║   ██║     ██║██║╚██╗██║██╔══╝  
+██║  ██║╚██████╔╝   ██║   ███████╗██║██║ ╚████║███████╗
+╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝
 ''')
 
+print(" Made by Hanako ")
 print(f'{Fore.RED}           ╔══════════════════════════╗')
 print(f'{Fore.RED}           ║{Fore.WHITE}   [1] Enter Name Info    {Fore.RED}║')
 print(f'{Fore.RED}           ║{Fore.WHITE}   [2] Enter Phone Number {Fore.RED}║')
@@ -28,24 +30,32 @@ if menu == "1":
     firstname = input("First name: ")
     lastname = input("Last name: ")
     location = input("City/State/Zip: ")
-    print(f"\nMock search link: https://example.com/name/{firstname}-{lastname}_{location}")
+    url = f"https://example.com/name/{firstname}-{lastname}_{location}"
+    print(f"\nOpening: {url}")
+    webbrowser.open(url)
 
 elif menu == "2":
     part1 = input("First 3 digits: ")
     part2 = input("Next 3 digits: ")
     part3 = input("Last 4 digits: ")
-    print(f"\nMock phone search: https://example.com/phone/{part1}-{part2}-{part3}")
+    url = f"https://example.com/phone/{part1}-{part2}-{part3}"
+    print(f"\nOpening: {url}")
+    webbrowser.open(url)
 
 elif menu == "3":
     house = input("House number: ")
     street = input("Street: ")
     city = input("City: ")
     state = input("State: ")
-    print(f"\nMock address search: https://example.com/address/{house}-{street}_{city}-{state}")
+    url = f"https://example.com/address/{house}-{street}_{city}-{state}"
+    print(f"\nOpening: {url}")
+    webbrowser.open(url)
 
 elif menu == "4":
     ip = input("IP address: ")
-    print(f"\nMock IP lookup: https://example.com/ip/{ip}")
+    url = f"https://example.com/ip/{ip}"
+    print(f"\nOpening: {url}")
+    webbrowser.open(url)
 
 else:
     print(f"{Fore.RED}[!] Invalid selection.")
