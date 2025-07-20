2082a97efd481cfb8f362f814bd2982616c4bba9
HanakoRico <joeyc5410@gmail.com>
2025-07-20 08:14:24 -0500

    Update and rename 6RKS X 988 Osint.py to 6RKS X 988  osint.py

--- 6RKS X 988  osint.py
+++ 6RKS X 988  osint.py
@@ -41,7 +41,7 @@ elif menu == "2":
     part1 = input("First 3 digits: ")
     part2 = input("Next 3 digits: ")
     part3 = input("Last 4 digits: ")
-    url = f"https://www.beenverified.com/?utm_source=brave&utm_medium=cpc&utm_campaign=BV_PPL_SEA_PRP_PPS_Brave_Peace_Of_Mind&utm_querytype=nonbrand{part1}-{part2}-{part3}"
+    url = fhttps://www.beenverified.com/reverse-phone/{part1}-{part2}-{part3}"
     print(f"\nOpening: {url}")
     webbrowser.open(url)
 
@@ -62,7 +62,7 @@ elif menu == "4":
 
 elif menu == "5":
     ip = input("Email ")
-    url = f"https://www.beenverified.com/?utm_querytype=nonbrand"
+    url = f"https://www.beenverified.com/email-search/?utm_source=brave&utm_medium=cpc&utm_campaign=BV_EMA_SEA_BRD_BDT_Brave_Search_Email_Address&utm_querytype=nonbrand"
     print(f"\nOpening: {url}")
     webbrowser.open(url)
 
